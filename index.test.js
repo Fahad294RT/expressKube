637ad3bc0ce60b5eb4da5535217e94ba37444f4f
Fahad Khan <fahad@rebeltechnology.io>
2021-07-29 15:53:53 +0500

    Artifacts test

---
+++ index.test.js
@@ -0,0 +1,13 @@
+
+/**
+ * clear && npx jest --no-cache --verbose -t "E2E"
+ */
+describe("E2E", () => {
+
+  beforeAll(() => {
+  });
+
+  it("Test code", () => {
+    expect(1).toBeDefined();
+  });
+});
