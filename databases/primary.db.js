a7396d55bb68919b08cca72344e4842553f30022
Fahad Khan <fahad@rebeltechnology.io>
2021-10-27 11:52:22 +0500

    Get paginated user.

--- databases/primary.db.js
+++
@@ -1,6 +0,0 @@
-const Sequelize= require("sequelize");
-
-const sequelize = new Sequelize('expressKube', 'root', 'root!@!@!@', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const models= require("./models");
+
+app.get('/', async (req, res) => {
+  res.json(await models.User.findAll({
+    offset: 0,
+    limit: 21
+  }))
 })
 
 app.listen(port, () => {
